a6f58d8ada23d2e9bc87cf2acc8e07d8fc6c6d09
unknown <riasat01@outlook.com>
2023-09-29 01:09:03 +0600

    forms added

--- src/pages/hero-register/HeroRegister.jsx
+++ src/pages/hero-register/HeroRegister.jsx
@@ -1,7 +1,49 @@
+import { useState } from "react";
+
 const HeroRegister = () => {
+
+
+    const [email, setEmail] = useState(``);
+    const [password, setPassword] = useState(``);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(e.target.email.value, e.target.password.value);
+        setEmail(e.target.email.value);
+        setPassword(e.target.password.value)
+    }
     return (
-        <div>
-            
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                    <form onSubmit={handleSubmit}>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" name="email" placeholder="email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Register</button>
+                    </div>
+                </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 const Login = () => {
+
+    const [email, setEmail] = useState(``);
+    const [password, setPassword] = useState(``);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(e.target.email.value, e.target.password.value);
+        setEmail(e.target.email.value);
+        setPassword(e.target.password.value)
+    }
     return (
-        <div>
-            
+        <div className="card flex-shrink-0 w-full max-w-2xl shadow-2xl bg-base-100 m-auto">
+            <div className="card-body">
+                <form onSubmit={handleSubmit}>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" name="email" placeholder="email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Login</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
